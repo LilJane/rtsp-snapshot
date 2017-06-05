2d56ce119d96f783a64a6d8be35fd200315bc234
Elizarov Maksim <info@LilJane.ru>
2017-06-05 12:16:36 +0500

    start.py
    The script for saving snapshots from the video in RTSP. Python version

---
+++ start.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import datetime
+import threading
+
+def download_file(id_name,ip):
+  filename = id_name + ".jpg"
+  retryes = 1
+  filesize = 0
+  directory = "ENTER DIRESTORY PATCH HERE, Example /var/www/rtsp/"
+  while retryes<12 and filesize==0: #number of attempts = 11
+    try:
+      if os.path.exists(directory + filename):
+        os.remove(directory + filename)
+        print(filename + ' remove')
+      print('Retryes ' + str(retryes))
+      os.system("/usr/local/bin/ffmpeg -i "+ ip +" -vframes 1 -r 3 -timeout 1 "+ directory + filename)
+      print(filename + " save")
+      size = os.stat(directory + filename)
+      filesize = size.st_size
+
+    except Exception as e: print(e)
+    retryes += 1
+devices = [
+
+{'id_name':'NAME','ip':'rtsp://ip:554'},
+{'id_name':'NAME','ip':'rtsp://ip:554'}
+]
+
+print("[-------------------------------- [" + str(datetime.datetime.now()) + "] START --------------------------------]")
+
+for i in xrange(0, len(devices)):
+	t1 = threading.Thread(target=download_file, args=(devices[i]['id_name'], devices[i]['ip']))
+	t1.start()
+
+print("[-------------------------------- [" + str(datetime.datetime.now()) + "] END --------------------------------]")
